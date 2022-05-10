0e38f36f7838360cdcf58bdb3a8081f2146715d6
Wans20 <wans200102@gmail.com>
2022-05-10 15:56:48 +0700

    bismillah final

--- admin/home.php
+++ admin/home.php
@@ -36,7 +36,7 @@ require_once("../config/config.php");
             <div class="d-flex">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">logout</a>
+                        <a class="nav-link active" aria-current="page" href="../">logout</a>
                     </li>
                 </ul>
       </div>
