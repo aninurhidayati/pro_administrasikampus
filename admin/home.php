638f0b70a5880ac43b11436da92db5a444e6c065
Wans20 <wans200102@gmail.com>
2022-05-11 14:01:39 +0700

    update

--- admin/home.php
+++ admin/home.php
@@ -29,14 +29,14 @@ require_once("../config/config.php");
             <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        <a class="nav-link active" aria-current="page" href="#"><i class="bi bi-house-door-fill"></i> Home</a>
                     </li>
                 </ul>
             </div>
             <div class="d-flex">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="../">logout</a>
+                        <a class="nav-link active" aria-current="page" href="../"><i class="bi bi-power"></i> Log Out</a>
                     </li>
                 </ul>
       </div>
